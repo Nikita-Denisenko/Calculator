08393580442c2eab99510b7424b56a750c7dd138
Nikita Denisenko <barba_nik92@mail.ru>
2025-05-17 11:16:29 +0300

    Implemented support for multi-operation expressions; no priority yet

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Dictionary<string, Func<double, double,  double>> operations = new()
+﻿Dictionary<string, Func<double, double,  double>> doOperation = new()
 {
     ["+"] = (a, b) => a + b,
     ["-"] = (a, b) => a - b,
@@ -6,43 +6,83 @@
     ["/"] = (a, b) => a / b,
 };
 
-var userString = Console.ReadLine();
-
-while (true)
+void Main()
 {
+    var expression = GetUserExpression();
     
-    if (!string.IsNullOrEmpty(userString))
+    while (!IsValid(expression))
     {
-        break;
+        expression = GetUserExpression();
     }
-    
-    userString = Console.ReadLine();
 
-}
+    var answer = Calculate(expression);
 
-var expression = userString.Split(" ");
-var s1 = expression[0]; 
-var s2 = expression[2];
-if (!(double.TryParse(s1, out var x) && double.TryParse(s2, out var y)))
+    Console.WriteLine(answer);
+} 
+
+string[] GetUserExpression()
 {
-    Console.WriteLine("Ошибочный ввод!");
-    return;
+    while (true)
+    {
+        var userString = Console.ReadLine();
+        
+        if (string.IsNullOrEmpty(userString))
+        {
+            continue;
+        }
+
+        return userString.Split(" ");
+    }
 }
 
-var operation = expression[1];
-
-if (!operations.Keys.ToArray().Contains(operation))
+bool IsValid(string[] input)
 {
-    Console.WriteLine("Ошибочный ввод!");
-    return;
+    for (var i = 0; i < input.Length; i++)
+    {
+        var current = input[i];
+        
+        if (i % 2 == 0)
+        {
+            if (!double.TryParse(current, out var _))
+            {
+                return false;
+            }
+
+            if (i > 0 && current == "0" && input[i - 1] == "/")
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+                return false;
+            }
+            continue;
+        }
+
+        if (!doOperation.ContainsKey(current))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
-if (operation == "/" && y == 0)
+double Calculate(string[] expression)
 {
-    Console.WriteLine("На ноль делить нельзя!");
-    return;
+    double result = 0;
+
+    for (var i = 1; i < expression.Length; i += 2)
+    {
+        var operation = expression[i];
+        var nextNumber = double.Parse(expression[i + 1]);
+        var lastNumber = result;
+        
+        if (i == 1)
+        {
+            lastNumber = double.Parse(expression[i - 1]);
+        }
+
+        result = doOperation[operation](lastNumber, nextNumber);
+    }
+
+    return result;
 }
 
-double result = operations[operation](x, y);
-
-Console.WriteLine(result); 
+Main();
